4296a3b4db21c43f5337222cc14575cf18773880
Daniela Miranda de Almeida <danielamirandaa9@gmail.com>
2022-03-02 15:35:52 -0300

    Criação do componente do angular

---
+++ primeiro-projeto/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+
+//componentes
+import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component'; //importação
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component', //nome da tag <>
+    template: `<p> Meu primeiro component com Angular`, //posso apontar um arquivo do html
+
+})
+export class MeuPrimeiroComponent{}
